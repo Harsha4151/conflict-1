efed0520d63af6c5045894e0c43b1d12cb7b58b1
Harsha4151 <hkamble4151@gmail.com>
2023-02-02 22:36:04 +0530

    changes by harsha

---
+++ src/main/java/com/BikkadIT/Arithmetic7Application.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Arithmetic7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Arithmetic7Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/BikkadIT/Demo.java
@@ -0,0 +1,8 @@
+package com.BikkadIT;
+
+public class Demo {
+	public void demo () {
+		System.out.println("demo of confict");
+	}
+
+}
